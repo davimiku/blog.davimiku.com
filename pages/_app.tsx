6d09048579f64fdb315d9a08aed578b92880c5dd
David Mikulis <davidmikulis@gmail.com>
2021-06-01 22:30:07 -0500

    Add prism theme for code styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
-import '../styles/globals.css'
+import 'prism-themes/themes/prism-vsc-dark-plus.css'
+import '../styles/globals.scss'
 import Layout from '../layouts'
 
 function MyApp({ Component, pageProps }) {
   return (
-  <Layout title='Blog' description='My Blog'>
-  <Component {...pageProps} />
-  </Layout>
+    <Layout title='Blog' description='My Blog'>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
 
