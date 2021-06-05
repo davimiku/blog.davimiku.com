1c4f9bf606786cc2bd882794cab83c296dcdaf32
David Mikulis <davidmikulis@gmail.com>
2021-06-04 20:56:19 -0500

    Add additional metadata and styling for the blog

--- layouts/blog/index.tsx
+++ layouts/blog/index.tsx
@@ -2,11 +2,14 @@ import { NextSeo } from 'next-seo'
 import { ReactNode } from 'react'
 import { FeaturedImage } from '../../components/blog/FeaturedImage'
 
-import styles from './mdx.module.scss'
+import styles from './blog.module.scss'
 
 type FrontMatter = {
   title: string
-  description: string
+  tagline: string
+  publishedAt: Date
+  tags?: string[]
+  __resourcePath: string
 }
 
 export type BlogsPageProps = {
@@ -15,13 +18,13 @@ export type BlogsPageProps = {
 }
 
 export default function BlogsPage({ children, frontMatter }: BlogsPageProps) {
-  const { title, description } = frontMatter
+  const { title, tagline } = frontMatter
   return (
     <>
-      <NextSeo title={title} description={description} />
+      <NextSeo title={title} description={tagline} />
       <article className={styles.container}>
         <h1>{title}</h1>
-        <p>{description}</p>
+        <p>{tagline}</p>
         <FeaturedImage title={title} />
         <section>{children}</section>
       </article>
