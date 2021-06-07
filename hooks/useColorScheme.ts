e4837ab2096910cd724cc4ff50c49121d56e24b6
David Mikulis <davidmikulis@gmail.com>
2021-06-07 17:29:55 -0500

    Add a hook to return the "dark" version of a component classname

--- hooks/useColorScheme.ts
+++ hooks/useColorScheme.ts
@@ -1,9 +1,51 @@
 import React from 'react'
-import { ColorSchemeContext } from '../contexts/color_scheme/ColorSchemeProvider'
+import classnames from 'classnames'
+
+import { ColorSchemeContext } from 'contexts/color_scheme'
+
+export type ScssModuleStyles = {
+  readonly [key: string]: string
+}
 
 /**
  * Provides the current color scheme value and a function to toggle the value.
  */
 export function useColorScheme() {
   return React.useContext(ColorSchemeContext)
+}
+
+/**
+ * Returns the classname for use in the element which automatically
+ * checks for the darkmode theme and returns the appropriate class.
+ *
+ * Pass the `styles` object that is imported from the SCSS Module
+ * and the class name which will automatically adjust for dark theme.
+ *
+ * For example:
+ * ```scss
+ * main {
+ *   padding: 0 5rem;
+ * }
+ *
+ * main--dark {
+ *  background-color: black;
+ *  color: white;
+ * }
+ * ```
+ *
+ * and in the component:
+ * ```ts
+ * import styles from './Main.module.scss'
+ *
+ * const mainClassName = useColorSchemeClass(styles, 'main')
+ * ```
+ */
+export function useColorSchemeClass(
+  styles: ScssModuleStyles,
+  className: string
+): string {
+  const { colorScheme } = useColorScheme()
+  return classnames(styles[className], {
+    [styles[className + '--dark']]: colorScheme === 'dark',
+  })
 }
