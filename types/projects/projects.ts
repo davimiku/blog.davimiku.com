7c5415c336396f646c1cc0457dd7b50b89c7fe6f
David Mikulis <davidmikulis@gmail.com>
2021-06-18 12:15:25 -0500

    Add data types for repositories and projects

---
+++ types/projects/projects.ts
@@ -0,0 +1,10 @@
+export type ProjectInfo = {
+  /** Short, catchy description of the project */
+  tagline: string
+
+  /** Technologies / languages / frameworks / tools used in this project */
+  technologies: string[]
+
+  /** Topics utilized or learned in this project */
+  topics: string[]
+}
