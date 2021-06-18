b84d7f13c27df24f09eb546896e8bb86096e3cac
David Mikulis <davidmikulis@gmail.com>
2021-06-18 14:28:16 -0500

    Add subcomponents to the Card

--- components/cards/card/Card.tsx
+++ components/cards/card/Card.tsx
@@ -4,5 +4,26 @@ import styles from './Card.module.scss'
 
 export function Card({ children }: PropsWithChildren<{}>) {
   const cardClass = useColorSchemeClass(styles, 'card')
-  return <div className={cardClass}>{children}</div>
+  return <article className={cardClass}>{children}</article>
+}
+
+export type CardTitleProps = {
+  title: string
+}
+
+Card.Header = function ({ children }: PropsWithChildren<{}>) {
+  return <header className={styles['card-header']}>{children}</header>
+}
+
+Card.Title = function ({ title }: CardTitleProps) {
+  return <h2>{title}</h2>
+}
+
+Card.Body = function ({ children }: PropsWithChildren<{}>) {
+  return <section className={styles['card-body']}>{children}</section>
+}
+
+Card.Footer = function ({ children }: PropsWithChildren<{}>) {
+  const className = useColorSchemeClass(styles, 'card-footer')
+  return <footer className={className}>{children}</footer>
 }
