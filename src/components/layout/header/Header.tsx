791434b5630b6ab74eb33fc4fda68bc234f51e00
David Mikulis <davidmikulis@gmail.com>
2021-06-26 16:33:54 -0500

    ♻️ Refactor each page to use Layout
    * Each page can now control its title and metadata
    * Standardize loading of font-awesome

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import Headroom from 'react-headroom'
 import { ThemeToggle } from '../../toggle/Toggle'
 import styles from './Header.module.scss'
@@ -6,13 +7,14 @@ import styles from './Header.module.scss'
 export function Header() {
   return (
     <Headroom>
-      <nav className={styles.nav}>
+      <nav className={styles['nav']}>
         <Link href='/'>
           <a>
             <h1>davimiku</h1>
           </a>
         </Link>
         <ul className={styles['nav-links']}>
+          <HeaderNavLink href='/' linkText='home' />
           <HeaderNavLink href='/projects' linkText='projects' />
           <HeaderNavLink href='/blog' linkText='blog' />
         </ul>
@@ -30,10 +32,13 @@ export type HeaderNavLinkProps = {
 }
 
 export function HeaderNavLink({ href, linkText }: HeaderNavLinkProps) {
+  const router = useRouter()
+
+  const className = router.pathname === href ? styles['nav-link-active'] : ''
   return (
     <li>
       <Link href={href}>
-        <a>{linkText}</a>
+        <a className={className}>{linkText}</a>
       </Link>
     </li>
   )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,12 @@
 import 'prism-themes/themes/prism-vsc-dark-plus.css'
 import '../styles/globals.scss'
-import Layout from '../layouts'
 import { ColorSchemeProvider } from 'color_scheme'
 
 function MyApp({ Component, pageProps }) {
+  console.log(Component)
   return (
     <ColorSchemeProvider>
-      <Layout title='Blog' description='My Blog'>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </ColorSchemeProvider>
   )
 }
