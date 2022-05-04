52491f5593c120e36dff59f0c52f62092f24a5cb
David Mikulis <davidmikulis@gmail.com>
2022-05-03 20:58:09 -0500

    🐛 fix types on App component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import 'prism-themes/themes/prism-vsc-dark-plus.css'
 import '../styles/globals.scss'
 import { ColorSchemeProvider } from 'color_scheme'
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ColorSchemeProvider>
       <Component {...pageProps} />
