85808e04adef48b677181f9fb7c72305851aa4fa
David Mikulis <davidmikulis@gmail.com>
2022-07-23 11:32:08 -0500

    👷 apply experimental property for unoptimized images
    Should allow the build to succeed for now, will optimize images once I have more images where it makes a difference

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const withPlugins = require('next-compose-plugins')
 const withSvgr = require('next-plugin-svgr')
 
 const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
     rehypePlugins: [],
@@ -16,12 +15,21 @@ const withMDX = require('@next/mdx')({
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = withPlugins([
-  withSvgr,
-  withMDX({
-    // Append the default value with md extensions
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  }),
-])
+const nextConfig = withPlugins(
+  [
+    withSvgr,
+    withMDX({
+      // Append the default value with md extensions
+      pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    }),
+  ],
+  {
+    experimental: {
+      images: {
+        unoptimized: true,
+      },
+    },
+  }
+)
 
 module.exports = nextConfig
