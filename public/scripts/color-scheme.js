ea2f6ca7eaa62d3266e595002f3b92a82b5bbaeb
davimiku <36610621+davimiku@users.noreply.github.com>
2024-12-04 20:16:11 -0600

    Remove dependencies on classnames and react-toggle
    Replace with native functionality where possible

--- public/scripts/color-scheme.js
+++ public/scripts/color-scheme.js
@@ -1,31 +1,57 @@
 // @ts-check
 
-let savedColorScheme = /** @type {"light" | "dark" | null} */ (
-  window.localStorage.getItem('color-scheme')
-)
-// if somehow this got set to anything but 'light' or 'dark'
-if (savedColorScheme && savedColorScheme !== 'light' && savedColorScheme !== 'dark') {
-  savedColorScheme = null
-  window.localStorage.removeItem('color-scheme')
-}
+const CSS_VAR_COLOR_SCHEME = '--color-scheme'
 
-if (savedColorScheme) {
-  setColorScheme(savedColorScheme)
-} else {
-  const lightMediaQuery = window.matchMedia('(prefers-color-scheme: light)')
-  if (lightMediaQuery.matches) {
-    setColorScheme('light')
-  } else {
-    setColorScheme('dark')
+window.addEventListener('DOMContentLoaded', () => {
+  const schemeToggle = /** @type {HTMLInputElement} */ (document.getElementById('scheme-toggle'))
+  schemeToggle.addEventListener('change', toggleColorScheme)
+
+  let savedColorScheme = /** @type {"light" | "dark" | null} */ (
+    window.localStorage.getItem('color-scheme')
+  )
+  // if somehow this got set to anything but 'light' or 'dark'
+  if (savedColorScheme && savedColorScheme !== 'light' && savedColorScheme !== 'dark') {
+    savedColorScheme = null
+    window.localStorage.removeItem('color-scheme')
   }
-}
+
+  if (savedColorScheme) {
+    setColorScheme(savedColorScheme)
+  } else {
+    const lightMediaQuery = window.matchMedia('(prefers-color-scheme: light)')
+    if (lightMediaQuery.matches) {
+      setColorScheme('light')
+    } else {
+      setColorScheme('dark')
+    }
+  }
+})
 
 /**
  * @param {"light" | "dark"} colorScheme
  */
 function setColorScheme(colorScheme) {
   const root = /** @type {HTMLElement} */ (document.querySelector(':root'))
+  const schemeToggle = /** @type {HTMLInputElement} */ (document.getElementById('scheme-toggle'))
   window.localStorage.setItem('color-scheme', colorScheme)
-  root.classList.remove('light', 'dark')
-  root.classList.add(colorScheme)
+  if (colorScheme === 'light') {
+    schemeToggle.checked = false
+    root.style.setProperty(CSS_VAR_COLOR_SCHEME, 'light')
+  } else {
+    schemeToggle.checked = true
+    root.style.setProperty(CSS_VAR_COLOR_SCHEME, 'dark')
+  }
+}
+
+function toggleColorScheme(event) {
+  const root = /** @type {HTMLElement} */ (document.querySelector(':root'))
+  const schemeToggle = /** @type {HTMLInputElement} */ (document.getElementById('scheme-toggle'))
+  const isDark = schemeToggle.checked
+  if (isDark) {
+    window.localStorage.setItem('color-scheme', 'dark')
+    root.style.setProperty(CSS_VAR_COLOR_SCHEME, 'dark')
+  } else {
+    window.localStorage.setItem('color-scheme', 'light')
+    root.style.setProperty(CSS_VAR_COLOR_SCHEME, 'light')
+  }
 }
--- src/components/color_scheme_toggle/ColorSchemeToggle.tsx
+++ src/components/color_scheme_toggle/ColorSchemeToggle.tsx
@@ -1,49 +1,19 @@
-import Toggle from 'react-toggle'
-
 import styles from './ColorSchemeToggle.module.scss'
-import React from 'react'
-
-type ColorScheme = 'light' | 'dark'
-
-function useColorScheme() {
-  const [colorScheme, setColorScheme] = React.useState<ColorScheme>('light')
-
-  function toggleColorScheme(newColorScheme?: ColorScheme) {
-    if (!newColorScheme) {
-      newColorScheme = colorScheme === 'light' ? 'dark' : 'light'
-    }
-
-    const root = document.querySelector(':root') as HTMLElement
-    root.classList.remove(colorScheme)
-
-    setColorScheme(newColorScheme)
-    window.localStorage.setItem('color-scheme', newColorScheme)
-    root.classList.add(newColorScheme)
-  }
-
-  React.useEffect(() => {
-    // 'color-scheme.js' guarantees this value is set
-    const colorScheme = window.localStorage.getItem('color-scheme') as ColorScheme
-    toggleColorScheme(colorScheme)
-  }, [])
-
-  return { colorScheme, toggleColorScheme }
-}
 
 export function ColorSchemeToggle() {
-  const { colorScheme, toggleColorScheme } = useColorScheme()
   return (
-    <div className={styles['toggle-container']}>
+    <div className={styles['container']}>
       <div className={`${styles['sun']} ${styles['icon']}`} />
-
-      <label className={styles['toggle-label']}>
-        <Toggle
-          checked={colorScheme === 'dark'}
-          onChange={() => toggleColorScheme()}
-          icons={false}
+      <label className={styles['toggle']}>
+        <input
+          type='checkbox'
+          name='scheme-toggle'
+          id='scheme-toggle'
+          className={styles['toggle__input']}
+          aria-label='Toggle light and dark mode'
         />
+        <span className={styles['toggle__display']} hidden></span>
       </label>
-
       <div className={`${styles['moon']} ${styles['icon']}`} />
     </div>
   )
