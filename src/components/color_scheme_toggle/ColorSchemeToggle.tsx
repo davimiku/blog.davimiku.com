5fc1c0e4f9b888906cd6d84fada5212ef3d5bd41
David <36610621+davimiku@users.noreply.github.com>
2024-12-12 07:31:35 -0600

    Fix toggle transition on route navigation
    In dark mode, the toggle kept resetting to the left and sliding over to the right on route navigation
    
    Since in Next.js `Link` we're not doing a full page refresh, we can keep the toggle value in state to avoid it changing. Full page refreshes will still show a brief slide over, which isn't as jarring since the whole page is loading anyways.

--- src/components/color_scheme_toggle/ColorSchemeToggle.tsx
+++ src/components/color_scheme_toggle/ColorSchemeToggle.tsx
@@ -1,31 +1,30 @@
-import { ChangeEvent, useEffect, useRef } from 'react'
+'use client'
+
+import { ChangeEvent, useLayoutEffect, useState } from 'react'
 import styles from './ColorSchemeToggle.module.scss'
 
-const CSS_VAR_COLOR_SCHEME = '--color-scheme'
+const CSS_VAR = '--color-scheme'
+const LOCAL_STORAGE_KEY = 'color-scheme'
 
 export function ColorSchemeToggle() {
-  const inputRef = useRef<HTMLInputElement>(null)
+  const [isDarkMode, setIsDarkMode] = useState(getSavedColorScheme() === 'dark')
 
-  function handleChange(event: ChangeEvent<HTMLInputElement>) {
-    const newScheme = event.target.checked ? 'dark' : 'light'
-    setColorScheme(newScheme, inputRef.current!)
-  }
-
-  useEffect(() => {
-    const input = inputRef.current!
-    let colorScheme = getSavedColorScheme()
-    if (!colorScheme) {
-      colorScheme = prefersDarkMode() ? 'dark' : 'light'
-    }
-    setColorScheme(colorScheme, input)
+  useLayoutEffect(() => {
+    const colorScheme = getSavedColorScheme()
+    setIsDarkMode(colorScheme === 'dark')
   }, [])
+
+  function handleChange({ target: { checked } }: ChangeEvent<HTMLInputElement>) {
+    setIsDarkMode(checked)
+    setGlobalColorScheme(checked ? 'dark' : 'light')
+  }
 
   return (
     <div className={styles['container']}>
       <div className={`${styles['sun']} ${styles['icon']}`} />
       <label className={styles['toggle']}>
         <input
-          ref={inputRef}
+          checked={isDarkMode}
           type='checkbox'
           name='scheme-toggle'
           id='scheme-toggle'
@@ -39,33 +38,42 @@ export function ColorSchemeToggle() {
   )
 }
 
-function getSavedColorScheme(): 'light' | 'dark' | null {
-  const savedColorScheme = window.localStorage.getItem('color-scheme')
+function getSavedColorScheme(): 'light' | 'dark' {
+  if (typeof window === 'undefined') return 'light'
+
+  const savedColorScheme = window.localStorage.getItem(LOCAL_STORAGE_KEY)
   if (savedColorScheme && (savedColorScheme === 'light' || savedColorScheme === 'dark')) {
     return savedColorScheme
   }
-  // if somehow this got set to anything but 'light' or 'dark'
+
+  // if somehow this got set to anything but 'light' or 'dark' clear it
   if (savedColorScheme) {
-    window.localStorage.removeItem('color-scheme')
+    window.localStorage.removeItem(LOCAL_STORAGE_KEY)
   }
 
-  return null
+  // fallback to user's preferences
+  if (prefersDarkMode()) {
+    setGlobalColorScheme('dark')
+    return 'dark'
+  } else {
+    setGlobalColorScheme('light')
+    return 'light'
+  }
 }
 
 function prefersDarkMode(): boolean {
+  if (typeof window === 'undefined') return false
+
   const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
   return mediaQuery.matches
 }
 
-function setColorScheme(colorScheme: 'light' | 'dark', input: HTMLInputElement) {
+/**
+ * Set localStorage and DOM manipulation outside of React's purview
+ */
+function setGlobalColorScheme(colorScheme: 'light' | 'dark') {
+  window.localStorage.setItem(LOCAL_STORAGE_KEY, colorScheme)
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- root will exist
   const root: HTMLElement = document.querySelector(':root')!
-  window.localStorage.setItem('color-scheme', colorScheme)
-  if (colorScheme === 'light') {
-    input.checked = false
-    root.style.setProperty(CSS_VAR_COLOR_SCHEME, 'light')
-  } else {
-    input.checked = true
-    root.style.setProperty(CSS_VAR_COLOR_SCHEME, 'dark')
-  }
+  root.style.setProperty(CSS_VAR, colorScheme)
 }
